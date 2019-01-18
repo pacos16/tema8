96a45d1a95f34d136d19e2129e958e86257d36b5
Paco Signes <pacos16@gmail.com>
2019-01-18 20:15:09 +0100

    PSA divendres 8

--- src/main/java/com/pacosignes/tema8/ex6/Bicicleta.java
+++ src/main/java/com/pacosignes/tema8/ex6/Bicicleta.java
@@ -1,4 +1,27 @@
 package com.pacosignes.tema8.ex6;
 
 public class Bicicleta {
+    private int referencia;
+    private String nombre;
+    private String modelo;
+    private float peso;
+    private float pulgadas;
+    private boolean tieneMotor;
+    private String fecha;
+    //todo gregorian calendar
+    private float precio;
+    private int existencias;
+
+//setters
+
+    public void setReferencia(int referencia){
+        this.referencia=referencia;
+
+    }
+    public boolean setNombre(String nombre){
+        this.nombre=nombre;
+        return true;
+
+    }
+
 }
--- src/main/java/com/pacosignes/tema8/ex6/Ex6.java
+++ src/main/java/com/pacosignes/tema8/ex6/Ex6.java
@@ -2,21 +2,33 @@ package com.pacosignes.tema8.ex6;
 
 
 import java.util.Scanner;
-public class ex6 {
+public class Ex6 {
+    public static void main(String[] args) {
+        int menu1;
+        Scanner lector = new Scanner(System.in);
 
-    int menu1;
-    Scanner lector = new Scanner(System.in);
+        System.out.println("***Gestion bicicletas***");
+        System.out.println("1.Anyadir bicicletas");
+        System.out.println("2.Vender bicicletas");
+        System.out.println("3.Consultar bicicletas");
+        System.out.println("3.Consultar stock");
+        System.out.println("-------------------------");
+        System.out.println("0. Salir");
 
-    System.out.println("***Gestion bicicletas***");
-    System.out.println("1.Anyadir bicicletas");
-    System.out.println("2.Vender bicicletas");
-    System.out.println("3.Consultar bicicletas");
-    System.out.println("3.Consultar stock");
-    System.out.println("-------------------------");
-    System.out.println("0. Salir");
 
-    menu1=lector.nextInt();
-    lector.nextLine();
+        menu1=lector.nextInt();
+        lector.nextLine();
+
+
+        switch (menu1){
+            case 0:
+                System.out.println("Hasta luego");
+                break;
+            case 1:
+                break;
+        }
+
+    }
 
 
 
