ccc9b7a4f26c6528cf6753abf4bed8f7c4e977eb
Paco Signes <pacos16@gmail.com>
2019-01-18 19:29:51 +0100

    fuck you

---
+++ src/main/java/com/pacosignes/tema8/ex6/Ex6.java
@@ -0,0 +1,24 @@
+package com.pacosignes.tema8.ex6;
+
+
+import java.util.Scanner;
+public class ex6 {
+
+    int menu1;
+    Scanner lector = new Scanner(System.in);
+
+    System.out.println("***Gestion bicicletas***");
+    System.out.println("1.Anyadir bicicletas");
+    System.out.println("2.Vender bicicletas");
+    System.out.println("3.Consultar bicicletas");
+    System.out.println("3.Consultar stock");
+    System.out.println("-------------------------");
+    System.out.println("0. Salir");
+
+    menu1=lector.nextInt();
+    lector.nextLine();
+
+
+
+
+}
